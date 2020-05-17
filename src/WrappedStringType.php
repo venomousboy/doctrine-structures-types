4585cfa426dd42ce26b259c949ac8b5e190e38ad
Vladislav Deykhin <deykhin@gmail.com>
2020-05-17 23:48:28 +0300

    Update WrappedStringType.php

--- src/WrappedStringType.php
+++ src/WrappedStringType.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace LazyLemurs\DoctrineStructuresTypes;
+namespace Venomousboy\DoctrineStructuresTypes;
 
 use Venomousboy\Structures\WrappedString;
 use Doctrine\DBAL\Platforms\AbstractPlatform;
